16184fb9e2ddf77a2e89f2ebc2112814136b8509
Samuel Shen <yangbos@andrew.cmu.edu>
2026-02-23 15:33:48 -0500

    解决依赖 latest 的问题，锁定稳定版本

--- pingrad/src/pages/docs.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Redirect} from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-export default function DocsLandingRedirect(): React.JSX.Element {
-  const introUrl = useBaseUrl('/docs/intro');
-  return <Redirect to={introUrl} />;
-}
